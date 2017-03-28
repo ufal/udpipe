a2371124143fc6adf0644e0c7e6ecee7b341e3c7
Milan Straka <milan@strakovi.com>
2017-03-28 02:13:12 +0200

    Synchronize public API in src_lib_only/udpipe.h header.

--- src_lib_only/udpipe.h
+++ src_lib_only/udpipe.h
@@ -31,10 +31,34 @@ struct string_piece {
 }
 typedef utils::string_piece string_piece;
 
-class word {
+class token {
  public:
+  std::string form;
+  std::string misc;
+
+  token(string_piece form = string_piece(), string_piece misc = string_piece());
+
+  // CoNLL-U defined SpaceAfter=No feature
+  bool get_space_after() const;
+  void set_space_after(bool space_after);
+
+  // UDPipe-specific all-spaces-preserving SpacesBefore and SpacesAfter features
+  void get_spaces_before(std::string& spaces_before) const;
+  void set_spaces_before(string_piece spaces_before);
+  void get_spaces_after(std::string& spaces_after) const;
+  void set_spaces_after(string_piece spaces_after);
+  void get_spaces_in_token(std::string& spaces_in_token) const;
+  void set_spaces_in_token(string_piece spaces_in_token);
+
+  // UDPipe-specific TokenRange feature
+  bool get_token_range(size_t& start, size_t& end) const;
+  void set_token_range(size_t start, size_t end);
+};
+
+class word : public token {
+ public:
+  // form and misc are inherited from token
   int id;         // 0 is root, >0 is sentence word, <0 is undefined
-  std::string form;    // form
   std::string lemma;   // lemma
   std::string upostag; // universal part-of-speech tag
   std::string xpostag; // language-specific part-of-speech tag
@@ -42,25 +66,19 @@ class word {
   int head;       // head, 0 is root, <0 is undefined
   std::string deprel;  // dependency relation to the head
   std::string deps;    // secondary dependencies
-  std::string misc;    // miscellaneous information
 
   std::vector<int> children;
 
-  word(int id = -1, string_piece form = string_piece()) : id(id), head(-1) {
-    if (form.len) this->form.assign(form.str, form.len);
-  }
+  word(int id = -1, string_piece form = string_piece()) : token(form), id(id), head(-1) {}
 };
 
-class multiword_token {
+class multiword_token : public token {
  public:
+  // form and misc are inherited from token
   int id_first, id_last;
-  std::string form;
-  std::string misc;
 
-  multiword_token(int id_first = -1, int id_last = -1, string_piece form = string_piece(), string_piece misc = string_piece()) : id_first(id_first), id_last(id_last) {
-    if (form.len) this->form.assign(form.str, form.len);
-    if (misc.len) this->misc.assign(misc.str, misc.len);
-  }
+  multiword_token(int id_first = -1, int id_last = -1, string_piece form = string_piece(), string_piece misc = string_piece())
+      : token(form, misc), id_first(id_first), id_last(id_last) {}
 };
 
 class empty_node {
@@ -88,11 +106,22 @@ class sentence {
   std::vector<std::string> comments;
   static const std::string root_form;
 
+  // Basic sentence modifications
   bool empty();
   void clear();
   word& add_word(string_piece form = string_piece());
   void set_head(int id, int head, const std::string& deprel);
   void unlink_all_words();
+
+  // CoNLL-U defined comments
+  bool get_new_doc(std::string* id = nullptr) const;
+  void set_new_doc(bool new_doc, string_piece id = string_piece());
+  bool get_new_par(std::string* id = nullptr) const;
+  void set_new_par(bool new_par, string_piece id = string_piece());
+  bool get_sent_id(std::string& id) const;
+  void set_sent_id(string_piece id);
+  bool get_text(std::string& text) const;
+  void set_text(string_piece text);
 };
 
 class input_format {
@@ -100,17 +129,24 @@ class input_format {
   virtual ~input_format() {}
 
   virtual bool read_block(std::istream& is, std::string& block) const = 0;
-  virtual void reset_document();
+  virtual void reset_document(string_piece id = string_piece()) = 0;
   virtual void set_text(string_piece text, bool make_copy = false) = 0;
   virtual bool next_sentence(sentence& s, std::string& error) = 0;
 
   // Static factory methods
   static input_format* new_input_format(const std::string& name);
-  static input_format* new_conllu_input_format();
-  static input_format* new_horizontal_input_format();
-  static input_format* new_vertical_input_format();
+  static input_format* new_conllu_input_format(const std::string& options = std::string());
+  static input_format* new_generic_tokenizer_input_format(const std::string& options = std::string());
+  static input_format* new_horizontal_input_format(const std::string& options = std::string());
+  static input_format* new_vertical_input_format(const std::string& options = std::string());
 
   static input_format* new_presegmented_tokenizer(input_format* tokenizer);
+
+  static const std::string CONLLU_V1;
+  static const std::string CONLLU_V2;
+  static const std::string GENERIC_TOKENIZER_NORMALIZED_SPACES;
+  static const std::string GENERIC_TOKENIZER_PRESEGMENTED;
+  static const std::string GENERIC_TOKENIZER_RANGES;
 };
 
 class output_format {
@@ -122,10 +158,17 @@ class output_format {
 
   // Static factory methods
   static output_format* new_output_format(const std::string& name);
-  static output_format* new_conllu_output_format();
-  static output_format* new_matxin_output_format();
-  static output_format* new_horizontal_output_format();
-  static output_format* new_vertical_output_format();
+  static output_format* new_conllu_output_format(const std::string& options = std::string());
+  static output_format* new_matxin_output_format(const std::string& options = std::string());
+  static output_format* new_horizontal_output_format(const std::string& options = std::string());
+  static output_format* new_plaintext_output_format(const std::string& options = std::string());
+  static output_format* new_vertical_output_format(const std::string& options = std::string());
+
+  static const std::string CONLLU_V1;
+  static const std::string CONLLU_V2;
+  static const std::string HORIZONTAL_PARAGRAPHS;
+  static const std::string PLAINTEXT_NORMALIZED_SPACES;
+  static const std::string VERTICAL_PARAGRAPHS;
 };
 
 class model {
@@ -140,10 +183,13 @@ class model {
   virtual bool parse(sentence& s, const std::string& options, std::string& error) const = 0;
 
   static const std::string DEFAULT;
+  static const std::string TOKENIZER_NORMALIZED_SPACES;
   static const std::string TOKENIZER_PRESEGMENTED;
+  static const std::string TOKENIZER_RANGES;
 };
 
 class pipeline {
+
  public:
   pipeline(const model* m, const std::string& input, const std::string& tagger, const std::string& parser, const std::string& output);
 
@@ -152,6 +198,9 @@ class pipeline {
   void set_tagger(const std::string& tagger);
   void set_parser(const std::string& parser);
   void set_output(const std::string& output);
+
+  void set_immediate(bool immediate);
+  void set_document_id(const std::string& document_id);
 
   bool process(std::istream& is, std::ostream& os, std::string& error) const;
 
