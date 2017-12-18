e5ec349f6c171e7dd65286ffa14123ebac615797
Milan Straka <milan@strakovi.com>
2017-12-18 10:38:36 +0100

    A Solaris compilation fix.
    This should fix #44.
    
    Also, it is a my attempt instead of #54.

--- src/tokenizer/multiword_splitter.cpp
+++ src/tokenizer/multiword_splitter.cpp
@@ -61,7 +61,7 @@ void multiword_splitter::append_token(string_piece token, string_piece misc, sen
   }
 
   // Determine casing
-  enum { UC_FIRST, UC_ALL, OTHER } casing = OTHER;
+  enum { UC_FIRST, UC_ALL, UC_OTHER }; int casing = UC_OTHER;
 
   if (unicode::category(utf8::first(token.str, token.len)) & unicode::Lut) {
     casing = UC_ALL;
