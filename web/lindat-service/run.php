0143858d0f08add645cc636d050339ad4bc02d4e
Milan Straka <milan@strakovi.com>
2018-01-31 14:15:36 +0100

    Pass data using multipart/form-data if supported.

--- web/lindat-service/run.php
+++ web/lindat-service/run.php
@@ -41,13 +41,22 @@ Documentation</a> and the models are described in the
     if (jQuery('#tagger').prop('checked')) options.tagger = "";
     if (jQuery('#parser').prop('checked')) options.parser = "";
 
+    var form_data = null;
+    if (window.FormData) {
+      form_data = new FormData();
+      for (var key in options)
+        form_data.append(key, options[key]);
+    }
+
     output_file_content = null;
     output_file_table = null;
     output_file_tree = null;
     jQuery('#submit').html('<span class="fa fa-cog"></span> Waiting for Results <span class="fa fa-cog"></span>');
     jQuery('#submit').prop('disabled', true);
     jQuery.ajax('//lindat.mff.cuni.cz/services/udpipe/api/process',
-           {dataType: "json", data: options, type: "POST", success: function(json) {
+           {data: form_data ? form_data : options, processData: form_data ? false : true,
+            contentType: form_data ? false : 'application/x-www-form-urlencoded; charset=UTF-8',
+            dataType: "json", type: "POST", success: function(json) {
       try {
         if ("result" in json) {
           output_file_content = json.result;
