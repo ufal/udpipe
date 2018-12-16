aaedebc7176c3ae208b26382ae543ce29c3cd3b8
Milan Straka <milan@strakovi.com>
2018-12-16 14:27:49 +0100

    Uncheck UD 2.0 (UD 2.3 is now the default).

--- web/lindat-service/run.php
+++ web/lindat-service/run.php
@@ -284,7 +284,7 @@ Documentation</a> and the models are described in the
         <label class="col-sm-2 control-label">Model:</label>
         <div class="col-sm-10">
           <label class="radio-inline"><input name="family" type="radio" value="-ud-2.3-" onchange="updateModels()" checked />UD 2.3 (<a href="http://ufal.mff.cuni.cz/udpipe/models#universal_dependencies_23_models">description</a>)</label>
-          <label class="radio-inline"><input name="family" type="radio" value="-ud-2.0-1" onchange="updateModels()" checked />UD 2.0 (<a href="http://ufal.mff.cuni.cz/udpipe/models#universal_dependencies_20_models">description</a>)</label>
+          <label class="radio-inline"><input name="family" type="radio" value="-ud-2.0-1" onchange="updateModels()" />UD 2.0 (<a href="http://ufal.mff.cuni.cz/udpipe/models#universal_dependencies_20_models">description</a>)</label>
           <label class="radio-inline"><input name="family" type="radio" value="-ud-1.2-" onchange="updateModels()" />UD 1.2 (<a href="http://ufal.mff.cuni.cz/udpipe/models#universal_dependencies_12_models">description</a>)</label>
         </div>
       </div>
