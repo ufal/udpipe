4aee095a6d0dbc808c653385129c00e89fcc8ebf
Milan Straka <milan@strakovi.com>
2019-06-05 10:54:11 +0200

    Remove UD 1.3 an UD 1.4 data files.

--- training/ud-1.3/merge_multiple.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf *_all/
-for lang in cs; do
-  mkdir -p ${lang}_all
-  for type in train dev test; do
-    cat $lang*/*$type*.conllu >${lang}_all/${lang}_all-ud-$type.conllu
-  done
-done
--- training/ud-1.3/stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for d in *; do
-  [ -d "$d" ] || continue
-
-  echo -n "$d"
-  ../scripts/nonprojective_stats.pl $d/*.conllu | sed 's/^/\t/' | tr -d "\n"
-  echo
-done
