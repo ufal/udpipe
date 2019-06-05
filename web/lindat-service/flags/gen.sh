fb36a747bee984bf5cd91b2a2ce6f6e6fe534a6d
Milan Straka <milan@strakovi.com>
2019-06-05 12:55:17 +0200

    Add UD 2.4 models to the service.

--- web/lindat-service/flags/gen.sh
+++ web/lindat-service/flags/gen.sh
@@ -4,7 +4,12 @@ set -e
 
 git clone --depth=1 --branch=pages-source https://github.com/UniversalDependencies/docs
 
-cat docs/gen_index/flags.json | perl -nle '/^"([^"]*)":"([^"]*)",?$/ and print lc($1) . " $2"' | while read name code; do
+(cat docs/gen_index/flags.json | perl -nle '/^"([^"]*)":"([^"]*)",?$/ and print lc($1) . " $2"'; cat <<EOF
+classical_chinese CN-QING
+old_russian RU-IVAN
+wolof SN
+EOF
+) | while read name code; do
   cp docs/flags/svg/$code.svg $name.svg
 done
 
