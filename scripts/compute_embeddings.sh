d4ab84018c0179d4ef96b56ff0d9cb8d0be86b07
Milan Straka <milan@strakovi.com>
2020-08-30 16:21:11 +0200

    Add script precomputing contextual embeddings.

---
+++ scripts/compute_embeddings.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+[ $# -ge 2 ] || { echo Usage: $0 data_directory embedding_args... >&2; exit 1; }
+data="$1"; shift
+
+for d in $data/*/; do
+  for f in $d*.conllu; do
+    qsub -p 0 -q gpu* -l gpu=1,mem_free=8G,h_data=16G -j y -o $f.log withcuda101 wembedding_service/venv/bin/python wembedding_service/compute_conllu_embeddings.py $f $f.npz "$@"
+  done
+done
