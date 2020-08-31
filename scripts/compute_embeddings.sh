55f921a074d224ec0b23c11ca99cc364dc6b7e9e
Milan Straka <milan@strakovi.com>
2020-08-31 09:14:55 +0200

    Adapt to the rename of the compute_conllu_embeddings script.

--- scripts/compute_embeddings.sh
+++ scripts/compute_embeddings.sh
@@ -5,6 +5,6 @@ data="$1"; shift
 
 for d in $data/*/; do
   for f in $d*.conllu; do
-    qsub -p 0 -q gpu* -l gpu=1,mem_free=8G,h_data=16G -j y -o $f.log withcuda101 wembedding_service/venv/bin/python wembedding_service/compute_conllu_embeddings.py $f $f.npz "$@"
+    qsub -p 0 -q gpu* -l gpu=1,mem_free=8G,h_data=16G -j y -o $f.log withcuda101 wembedding_service/venv/bin/python wembedding_service/compute_wembeddings.py --format=conllu $f $f.npz "$@"
   done
 done
