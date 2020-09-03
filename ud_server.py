a65b7d95aed59ed585d3aa043981a8e1a68999bc
Milan Straka <milan@strakovi.com>
2020-09-03 23:39:45 +0200

    Make sure SIGINT and SIGUSR1 are blocked before sigwait-ing them.

--- ud_server.py
+++ ud_server.py
@@ -349,6 +349,7 @@ if __name__ == "__main__":
     print("To stop it gracefully, either send SIGINT (Ctrl+C) or SIGUSR1.", file=sys.stderr, flush=True)
 
     # Wait until the server should be closed
+    signal.pthread_sigmask(signal.SIG_BLOCK, [signal.SIGINT, signal.SIGUSR1])
     signal.sigwait([signal.SIGINT, signal.SIGUSR1])
     print("Initiating shutdown of the UDPipe 2 server.", file=sys.stderr, flush=True)
     server.shutdown()
