0bdccaa6a38f97bfffb96fec5e7c37ce244ede70
Milan Straka <milan@strakovi.com>
2020-09-12 17:42:31 +0200

    Do not recompute embeddings newer than the CoNLL-U files.

--- scripts/compute_embeddings.sh
+++ scripts/compute_embeddings.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-[ $# -ge 2 ] || { echo Usage: $0 data_directory embedding_args... >&2; exit 1; }
+[ $# -ge 1 ] || { echo Usage: $0 data_directory embedding_args... >&2; exit 1; }
 data="$1"; shift
 
 for d in $data/*/; do
   for f in $d*.conllu; do
+    [ $f.npz -nt $f ] && continue
     qsub -p 0 -q gpu* -l gpu=1,mem_free=8G,h_data=16G -j y -o $f.log withcuda101 wembedding_service/venv/bin/python wembedding_service/compute_wembeddings.py --format=conllu $f $f.npz "$@"
   done
 done
