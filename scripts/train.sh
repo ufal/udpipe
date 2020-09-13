391aabed114088df5399894b1887853f1e8d7cab
Milan Straka <milan@strakovi.com>
2020-09-13 15:26:47 +0200

    Add la_evalatin20 to training scripts; support EXPeriments.

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,13 +4,11 @@
 data="$1"; shift
 treebank="$1"; shift
 
-mkdir -p models/$data-$treebank
+mkdir -p models/$data-$treebank${EXP:+-$EXP}
 size=$(grep -P "^$treebank\t" $data/langs_sizes | cut -f2)
 if [ "$size" -ge 400000 ]; then
-  gpu_ram=16G
   args=--batch_size=64
 else
-  gpu_ram=8G
   args=--batch_size=32
 fi
 if [ "$size" -ge 50000 ]; then
@@ -29,7 +27,14 @@ case $treebank in
       evaluation_data="$evaluation_data $(basename $(dirname $test)):$test"
     done
     ;;
+  la_evalatin20)
+    args="$args --tags=UPOS,LEMMAS --parse=0 --rnn_layers=3"
+    evaluation_data="--dev=$data/la_evalatin20/la_evalatin20-dev.conllu --test"
+    for kind in "" "-crossgenre" "-crosstime"; do
+      evaluation_data="$evaluation_data ${kind:+la_evalatin20$kind}:$data/la_evalatin20/la_evalatin20-test$kind.conllu"
+    done
+    ;;
   *) evaluation_data="--dev=$data/$treebank/$treebank-ud-dev.conllu --test=$data/$treebank/$treebank-ud-test.conllu"
 esac
 
-qsub -q gpu* -l gpu=1,gpu_ram=$gpu_ram,mem_free=16G,h_data=24G -j y -o models/$data-$treebank/training.log withcuda100 venv/bin/python ud_parser.py models/$data-$treebank --train=$data/$treebank/$treebank-ud-train.conllu $evaluation_data $args "$@"
+qsub $SGE_ARGS -q gpu* -l gpu=1,gpu_ram=11G,mem_free=16G,h_data=24G -N udpipe2 -j y -o models/$data-$treebank${EXP:+-$EXP}/training.log withcuda100 venv/bin/python ud_parser16.py models/$data-$treebank${EXP:+-$EXP} --train $data/$treebank/$treebank-*train.conllu $evaluation_data $args "$@"
--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-[ $# -ge 2 ] || { echo Usage: $0 datadir args... >&2; exit 1; }
+[ $# -ge 1 ] || { echo Usage: $0 datadir args... >&2; exit 1; }
 data="$1"; shift
 
 for treebank in $(cut -f1 $data/langs_sizes); do
-  sh train.sh $data $treebank "$@"
+  sh $(dirname $0)/train.sh $data $treebank "$@"
+  while [ `qstat | grep -c udpipe2` -ge `cat $(dirname $0)/train_all.limit` ]; do sleep 15; done
 done
