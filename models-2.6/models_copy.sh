9d15bddc1370835d296bf5e24bff393342b07a4a
Milan Straka <milan@strakovi.com>
2020-09-16 13:36:27 +0200

    Update the models.
    - Add la_evalatin20
    - Change list of models using the shared `-all` model
    - Rename the model directories

--- models-2.6/models_copy.sh
+++ models-2.6/models_copy.sh
@@ -8,9 +8,9 @@ for treebank in $(cut -f1 ../$data/langs_sizes); do
 
   lang=${treebank%%_*}
   if [ -d ../$data/${lang}_all ] && ! grep -q "^$treebank\$" models_non_shared.txt; then
-    rsync -av -m --delete --exclude *.v2 ../models/$data-${lang}_all/ $data-${lang}_all-200830.model
-    ln -fsn $data-${lang}_all-200830.model $data-$treebank-200830.model
+    rsync -av -m --delete --exclude *.v2 --exclude training.log ../models/$data-${lang}_all/ ${lang}_all-$data-200830.model
+    ln -fsn ${lang}_all-$data-200830.model $treebank-$data-200830.model
   else
-    rsync -av -m --delete --exclude *.v2 ../models/$data-$treebank/ $data-$treebank-200830.model
+    rsync -av -m --delete --exclude *.v2 --exclude training.log ../models/$data-$treebank/ $treebank-$data-200830.model
   fi
 done
