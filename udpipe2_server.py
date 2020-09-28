e0839eef31bba5961abc4a2313210cc318c3c9c2
Milan Straka <milan@strakovi.com>
2020-09-28 19:44:50 +0200

    Correct link to UDPipe 2 acknowledgements.

--- udpipe2_server.py
+++ udpipe2_server.py
@@ -292,7 +292,7 @@ class UDServer(socketserver.ThreadingTCPServer):
                                 request.respond("application/json")
                                 request.wfile.write(json.dumps({
                                     "model": model.names[0],
-                                    "acknowledgements": ["http://ufal.mff.cuni.cz/udpipe#udpipe_acknowledgements", model.acknowledgements],
+                                    "acknowledgements": ["http://ufal.mff.cuni.cz/udpipe/2#udpipe2_acknowledgements", model.acknowledgements],
                                     "result": "",
                                 }, indent=1)[:-3].encode("utf-8"))
                                 if output_format == "conllu":
