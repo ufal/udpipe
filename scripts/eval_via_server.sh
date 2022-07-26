93a4fbec2e85ce12261d4ecc8b69b4506114ced0
Milan Straka <milan@strakovi.com>
2022-07-26 23:15:48 +0200

    Update evaluation script.
    - Allow specifying a server to use.
    - Remove the MISC field from the gold CoNLL-U to circumvent the
      fact that in UDPipe 1.2 it cannot contain spaces.

--- scripts/eval_via_server.sh
+++ scripts/eval_via_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-[ $# -ge 2 ] || { echo "Usage: $0 model conllu_data [plain_text]" >&2; exit 1; }
+[ $# -ge 2 ] || { echo "Usage: $0 model conllu_data [plain_text] [server]" >&2; exit 1; }
 model="$1"
 data_conllu="$2"
 data_txt="$3"
+server="${4:-https://lindat.mff.cuni.cz/services/udpipe/api}"
 
-python3 $(dirname $0)/../udpipe2_eval.py -v $data_conllu <(curl -F model=$model -F data=@${data_txt:-$data_conllu} ${data_txt:+-F tokenizer=} -F tagger= -F parser= https://lindat.mff.cuni.cz/services/udpipe/api/process | jq -j .result)
+python3 $(dirname $0)/../udpipe2_eval.py -v $data_conllu <(sed "${data_txt:+p;d;}"'s/\t[^\t]*$/\t_/' ${data_txt:-$data_conllu} | curl -F model=$model -F data=@- ${data_txt:+-F tokenizer=} -F tagger= -F parser= $server/process | jq -j .result)
