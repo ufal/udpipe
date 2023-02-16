b06f385f12b56ea9714849afe7c5809807eb5485
Milan Straka <milan@strakovi.com>
2023-02-16 19:21:07 +0100

    Bump version to 1.3.1-dev.

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -19,7 +19,7 @@ namespace udpipe {
 
 // Returns current version.
 version version::current() {
-  return {1, 3, 0, ""};
+  return {1, 3, 1, "dev"};
 }
 
 // Returns multi-line formated version and copyright string.
