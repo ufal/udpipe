59b2d4eda6e3bc17f94105738bd96a3efa4a131c
Milan Straka <milan@strakovi.com>
2023-02-18 12:10:08 +0100

    Use stable MorphoDiTa 1.11.1.
    This does not really change anything, it is the same as the currently
    used 1.11.1-dev; it is just nicer to use a stable version.

--- scripts/update_morphodita.sh
+++ scripts/update_morphodita.sh
@@ -10,7 +10,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 rm -rf ../src/morphodita/*
-git -C ../src/morphodita clone --depth=1 --branch=master https://github.com/ufal/morphodita
+git -C ../src/morphodita clone --depth=1 --branch=stable https://github.com/ufal/morphodita
 cp -a ../src/morphodita/morphodita/src/{derivator,morpho,tagger,tagset_converter,tokenizer,version,Makefile.include} ../src/morphodita/
 cp -a ../src/morphodita/morphodita/{AUTHORS,CHANGES,LICENSE,README} ../src/morphodita/
 rm -rf ../src/morphodita/morphodita/
--- src/morphodita/version/version.cpp
+++ src/morphodita/version/version.cpp
@@ -17,7 +17,7 @@ namespace udpipe {
 namespace morphodita {
 
 version version::current() {
-  return {1, 11, 1, "dev"};
+  return {1, 11, 1, ""};
 }
 
 // Returns multi-line formated version and copyright string.
